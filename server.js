1992b58cb8b05fc951fce54bb5cbb5e147ef0ac4
Anand Singh <sanand926@gmail.com>
2018-10-08 19:57:59 +0530

    express middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 // create app
 
@@ -8,6 +9,22 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('unable to append file')
+        }
+    })
+    next()
+})
+
+// app.use((req, res, next) => {
+//     res.render('mentinance.hbs')
+// })
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
