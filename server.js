b0ee48d51587649aae41f29adef4f9501d61b83c
Anand Singh <sanand926@gmail.com>
2018-10-08 19:19:13 +0530

    advanced templating

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const hbs = require('hbs')
 
 // create app
 
-var app = express()
+var app = express();
 
-app.get('/', (req, res) => {
-    res.send('Heloo anand')
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
 })
 
-app.listen(3000)
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+    // res.send('Heloo anand')
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error:'Not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000')
+})
